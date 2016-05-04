68e17e8dad0e2063da134d7db8fd9b7b269fbeac
Sergi Maymi <smaymi@portic.net>
2016-05-04 08:58:21 +0200

    canvis servei camió

--- app/lorry/lorry.service.ts
+++ app/lorry/lorry.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from 'angular2/core';
 import {ILorryMovement} from './lorryMovement'
 import {Http, Response} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
-
+import {Jsonp, URLSearchParams} from '@angular/http';
 @Injectable()
 export class LorryService {
     private _lorryMovementsUrl = 'api/lorry/lorry-list.json'
@@ -12,6 +12,14 @@ export class LorryService {
     constructor(private _http: Http) {}
     
     getLorryMovements(payload) : Observable<ILorryMovement[]> {
+        let queryString = this.generateQueryString(payload);
+        
+        return this._http.get(this._lorryMovementsUrl)
+            .map((response: Response) => <ILorryMovement[]>response.json())
+            .catch(this.handleError)
+    }
+
+    postLorryMovements(payload) : Observable<ILorryMovement[]> {
         return this._http.post(this._lorryMovementsUrl, JSON.stringify(payload))
             .map((response: Response) => <ILorryMovement[]>response.json())
             .catch(this.handleError)
@@ -20,5 +28,15 @@ export class LorryService {
     private handleError(error: Response) {
         console.error(error);
         return Observable.throw(error.json().error || 'Server error');
+    }
+    
+    
+    generateQueryString(payload): string {
+        return "?tipoSelect="+payload.typeSelect
+        + "contenedor=" + payload.contenedor
+        + "paisSessio=" + payload.paisSessio
+        + "nifSessio=" + payload.nifSessio
+        + "usuariSessio=" + payload.usuariSessio
+        + "token=" + payload.token
     }        
 }
